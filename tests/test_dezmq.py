de6148dd6dc4bedc016dce3b3c0d3b2035db23f8
ffreemt <yucongo+fmt@gmail.com>
2022-12-03 11:48:01 +0800

    first commit

---
+++ tests/test_dezmq.py
@@ -0,0 +1,17 @@
+"""Test dezmq."""
+# pylint: disable=broad-except
+from dezmq import __version__
+from dezmq import dezmq
+
+
+def test_version():
+    """Test version."""
+    assert __version__[:3] == "0.1"
+
+
+def test_sanity():
+    """Check sanity."""
+    try:
+        assert not dezmq()
+    except Exception:
+        assert True
